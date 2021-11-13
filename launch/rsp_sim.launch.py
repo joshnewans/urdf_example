93bf1bb9da5217fe19e13c2f04942aec8f20e7be
Josh Newans <josh.newans@gmail.com>
2021-11-13 11:01:03 +1100

    Add Gazebo example

---
+++ launch/rsp_sim.launch.py
@@ -0,0 +1,58 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    # Specify the name of the package and path to xacro file within the package
+    pkg_name = 'urdf_example'
+    file_subpath = 'description/example_robot.urdf.xacro'
+
+
+    # Use xacro to process the file
+    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
+    robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+
+    # Configure the node
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_raw,
+        'use_sim_time': True}] # add other parameters here if required
+    )
+
+
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        )
+
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                    arguments=['-topic', 'robot_description',
+                                '-entity', 'my_bot'],
+                    output='screen')
+
+
+
+
+
+
+    # Run the node
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity
+    ])
+
+
